483096ffdaefc38ad7e77f21baa28c28378042ac
jon-z <jondashz@gmail.com>
2012-10-27 11:19:55 +1100

    Clean adsl keep alive script

--- adsl_auto_up.sh
+++ adsl_auto_up.sh
@@ -1,6 +1,9 @@
 #! /bin/bash
 #
-# Script to start pppoe session if it is not active or if there is not L3 connectivity
+# A blunt script to start pppoe session if it is not active or if there is no L3 connectivity
+
+KICK="/usr/bin/poff -a &> /dev/null && /usr/bin/pon dsl-provider &> /dev/null || /usr/bin/pon dsl-provider &> /dev/null"
+PEER=$( /bin/ip addr show ppp0 | /bin/grep inet | /usr/bin/cut -d " " -f8 | /usr/bin/cut -d / -f1 )
 
 # Check layer 2
 if [ $( /sbin/ip link show eth0 | /bin/grep UP &> /dev/null ; /bin/echo $? ) != 0 ]
@@ -10,21 +13,20 @@ fi
 
 if [ $( /sbin/ip link show | /bin/grep ppp &> /dev/null ; /bin/echo $? ) != 0 ]
 	then
-		$( /usr/bin/poff -a &> /dev/null && /usr/bin/pon dsl-provider &> /dev/null || /usr/bin/pon dsl-provider &> /dev/null )
+		$($KICK)
 	exit 1
 fi 
 
 # Check layer 3
 if [ $( /bin/ip addr show ppp0 | /bin/grep ppp0 &> /dev/null ; /bin/echo $? ) != 0 ]
 	then
-		$( /usr/bin/poff -a &> /dev/null && /usr/bin/pon dsl-provider &> /dev/null || /usr/bin/pon dsl-provider &> /dev/null )
+		$($KICK)
 	exit 1
 fi
 
-PEER=$( /bin/ip addr show ppp0 | /bin/grep inet | /usr/bin/cut -d " " -f8 | /usr/bin/cut -d / -f1 )
 if [ $( /bin/ping -c 1 "$PEER" &> /dev/null ; /bin/echo $? ) != 0  ]
 	then
-		$( /usr/bin/poff -a &> /dev/null && /usr/bin/pon dsl-provider &> /dev/null || /usr/bin/pon dsl-provider &> /dev/null )
+		$($KICK)
 	exit 1
 fi
 exit 0
